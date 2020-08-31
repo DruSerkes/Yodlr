3552e7de3706bc1f7f9af5fa76d87b89994c6e19
Dru Serkes <DruSerkes@gmail.com>
2020-08-31 15:24:33 -0400

    create Admin component

---
+++ frontend/src/yodlr/Admin.js
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect } from 'react';
+
+const BASE_URL = `http://localhost:3001/users`;
+
+const Admin = () => {
+	const { users, dispatch } = useContext(UserContext);
+
+	useEffect(
+		() => {
+			fetch(BASE_URL, { method: 'GET' })
+				.then((response) => response.json())
+				.then((data) => console.log(data))
+				.catch((err) => console.log(err));
+		},
+		[ users ]
+	);
+
+	return (
+		<div>
+			{users ? (
+				users.map((u) => (
+					<h4>
+						{u.email} {u.firstName} {u.lastName}
+					</h4>
+				))
+			) : (
+				<h3>Loading &hellip; </h3>
+			)}
+		</div>
+	);
+};
+
+export default Admin;
