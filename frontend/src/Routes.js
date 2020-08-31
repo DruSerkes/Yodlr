0743642897c0568a155943f288c3ec7356e085e7
Dru Serkes <DruSerkes@gmail.com>
2020-08-31 12:43:18 -0400

    add component comments for Routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -6,15 +6,19 @@ const Routes = () => {
 		<Switch>
 			<Route path="/signup">
 				<h1>Signup</h1>
+				{/* <Signup /> */}
 			</Route>
 			<Route path="/login">
 				<h1>Login</h1>
+				{/* <Login /> */}
 			</Route>
 			<Route path="/admin">
 				<h1>Admin</h1>
+				{/* <Admin /> */}
 			</Route>
 			<Route path="/">
 				<h1>Home</h1>
+				{/* <Home /> */}
 			</Route>
 			<Redirect to="/" />
 		</Switch>
