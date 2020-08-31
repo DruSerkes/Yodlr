62364d43264ca6d1c320efcc1cd1f92fecfa1b3e
Dru Serkes <DruSerkes@gmail.com>
2020-08-31 12:51:49 -0400

    create Home component

---
+++ frontend/src/yodlr/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Home = () => {
+	return (
+		<div className="Home">
+			<Typography variant="h1">Yodlr Design Challenge</Typography>
+			{/* TODO add logic to check if user is logged in and display different components/text */}
+		</div>
+	);
+};
+
+export default Home;
