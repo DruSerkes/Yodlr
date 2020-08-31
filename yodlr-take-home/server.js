cdc5a6f097ca2b0decfa89e827253d17149e870c
Dru Serkes <DruSerkes@gmail.com>
2020-08-31 03:14:39 -0400

    PORT variable retrieved from config

--- yodlr-take-home/server.js
+++ yodlr-take-home/server.js
@@ -1,7 +1,8 @@
 const app = require('./index');
+const { PORT } = require('./config');
 const logger = require('./lib/logger');
 const log = logger(app);
 
-const server = app.listen(app.get('port'), function() {
+const server = app.listen(PORT, () => {
 	log.info('Express server listening on http://localhost:%d', server.address().port);
 });
