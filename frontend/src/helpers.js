4dbae79b8c22e28a9bb9c7b574050fd7a937d3da
Dru Serkes <DruSerkes@gmail.com>
2020-09-01 13:45:01 -0400

    add helpers

---
+++ frontend/src/helpers.js
@@ -0,0 +1,15 @@
+export const BASE_URL = `http://localhost:3001/users`;
+
+export const addUserToDb = useCallback(async (values) => {
+	const response = await fetch(BASE_URL, {
+		method  : 'POST',
+		headers : {
+			'Content-Type' : 'application/json'
+		},
+		body    : {
+			user : values
+		}
+	});
+	const data = response.json();
+	return data;
+});
