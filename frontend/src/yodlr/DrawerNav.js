4f64d484bedfd29a177da9aee9e61b9c05180280
Dru Serkes <DruSerkes@gmail.com>
2020-09-02 01:43:48 -0400

    update copy. remove comments.

--- frontend/src/yodlr/DrawerNav.js
+++ frontend/src/yodlr/DrawerNav.js
@@ -3,7 +3,6 @@ import OpenContext from '../Context';
 import clsx from 'clsx';
 import { useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-// import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
@@ -13,21 +12,16 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-// import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
-// import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
-// import { NavLink as RouterLink } from 'react-router-dom';
 import useStyles from '../useStyles';
 import ListItemLink from './ListItemLink';
 
 const DrawerNav = () => {
 	const classes = useStyles();
 	const theme = useTheme();
-	// const [ open, setOpen ] = useState(false);
 	const { open, setOpen } = useContext(OpenContext);
 
 	const handleDrawerOpen = () => {
@@ -40,7 +34,6 @@ const DrawerNav = () => {
 
 	return (
 		<div>
-			{/* <CssBaseline /> */}
 			<AppBar
 				position="fixed"
 				className={clsx(classes.appBar, {
@@ -58,7 +51,7 @@ const DrawerNav = () => {
 						<MenuIcon />
 					</IconButton>
 					<Typography variant="h6" noWrap>
-						Persistent drawer
+						Yodlr
 					</Typography>
 				</Toolbar>
 			</AppBar>
@@ -90,7 +83,6 @@ const DrawerNav = () => {
 				</List>
 			</Drawer>
 		</div>
-		// {/* // </div> */}
 	);
 };
 
