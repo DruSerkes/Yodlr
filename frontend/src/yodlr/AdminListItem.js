9819815cf880d30f7cf1e48f607c204865952427
Dru Serkes <DruSerkes@gmail.com>
2020-09-01 22:02:31 -0400

    add handleActivate for AdminListItem

--- frontend/src/yodlr/AdminListItem.js
+++ frontend/src/yodlr/AdminListItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Avatar, ListItem, ListItemAvatar, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 
-const AdminListItem = ({ id, firstName, lastName, email, state, removeUser, classes }) => {
+const AdminListItem = ({ id, firstName, lastName, email, state, removeUser, classes, activateUser }) => {
 	const fullName = `${firstName} ${lastName}`;
 	const handleRemove = () => {
 		removeUser(id);
+	};
+	const handleActivate = () => {
+		const user = { id, firstName, lastName, email, state };
+		activateUser(user);
 	};
 	return (
 		<ListItem className={classes.listItem} key={id}>
@@ -20,8 +24,8 @@ const AdminListItem = ({ id, firstName, lastName, email, state, removeUser, clas
 				<ListItemText className={classes.userStatePending}>{state}</ListItemText>
 			)}
 			<ListItemSecondaryAction>
-				<IconButton edge="end" aria-label="activate">
-					<EditIcon />
+				<IconButton edge="end" aria-label="activate" onClick={handleActivate}>
+					<VpnKeyIcon />
 				</IconButton>
 				<IconButton edge="end" aria-label="delete" onClick={handleRemove}>
 					<DeleteIcon />
